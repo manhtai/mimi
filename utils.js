65669507272925616a67e885880e857dc72dffe8
manhtai <vo@manhtai.com>
2017-12-22 12:05:38 +0700

    Update initial comment

--- utils.js
+++ utils.js
@@ -10,22 +10,27 @@ const postImageToChannel = (team, channel, buffer, name, initial_comment) => {
   const imageUpload = "https://slack.com/api/files.upload";
   const token = config.SLACK_API_TOKEN[team.toLowerCase()];
   const fileName = `${name} ${moment().tz(config.TIME_ZONE).format("D-M-YYYY_HH.mm")} report.png`;
+  const formData = {
+      username: config.SLACK_NAME,
+      token: token,
+      file: {
+        value: buffer,
+        options: {
+          filename: fileName,
+          contentType: 'image/png'
+        }
+      },
+      filename: fileName,
+      channels: channel
+  };
+
+  if (initial_comment) {
+    formData.initial_comment = initial_comment;
+  }
+
   request.post({
       url: imageUpload,
-      formData: {
-        username: config.SLACK_NAME,
-        token: token,
-        file: {
-          value: buffer,
-          options: {
-            filename: fileName,
-            contentType: 'image/png'
-          }
-        },
-        filename: fileName,
-        initial_comment: initial_comment,
-        channels: channel
-      },
+      formData: formData,
     },
     (error, response, body) => {
       console.log(body);
